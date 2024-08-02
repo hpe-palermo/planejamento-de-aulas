03eff7b899baa67762e2cb07715574e37a94d0f4
Palermo <124798752+hpe-palermo@users.noreply.github.com>
2024-08-02 13:59:56 -0300

    Update NavBar.jsx

--- react-app/src/components/NavBar.jsx
+++ react-app/src/components/NavBar.jsx
@@ -3,16 +3,20 @@ import styled from "styled-components";
 
 const MenuToogle = styled.div`
     display: none;
+
+    @media (max-width: 1023px) {
+        display: inline
+    }
 `;
 
 function NavBar() {
     return (
-        <nav className="nav p-3 text-light" style={{backgroundColor: '#101010'}}>
-            <div className="container-fluid">
-                <MenuToogle className="menu-toggle active fs-4" id="open-sidebar">
+        <nav className="nav p-3 text-light w-100" style={{backgroundColor: '#101010'}}>
+            <div className="container-fluid d-flex justify-content-between">
+                <MenuToogle className="menu-toggle active fs-4 bg-sucess d-flex align-items-center" id="open-sidebar">
                     <i className="bi-list text-color"></i>
                 </MenuToogle>
-                <h1 className="navbar-brand mb-0 fs-4 text-color">Planejamento de Aulas</h1>
+                <h1 className="navbar-brand mb-0 fs-4 text-color bg-sucess d-flex align-items-center">Planejamento de Aulas</h1>
                 <div className="fs-4 text-color rounded-circle">
                     <a href="login.html" className="nav-link"><i className="bi-box-arrow-left"></i></a>
                 </div>
