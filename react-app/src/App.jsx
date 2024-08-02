69fdc78e67bf96caba09a4a882c1d9340090d67f
hpe-palermo <a>
2024-08-02 12:24:53 -0300

    react-app

---
+++ react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ react-app/src/components/Register.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Container = styled.div`
+    background-color: #dddcdc;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #000;
+    box-shadow: 0 0 10px #000;
+`;
+
+const Button = styled.button`
+    background: #101010;
+    border: none;
+    outline: none;
+    padding: 10px;
+    width: 100%;
+    border-radius: 10px;
+    color: #eee;
+    text-transform: uppercase;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+    transition: background-color 0.2s;
+
+    &:hover {
+        background-color: #333333;
+    }
+`
+
+const IconGoogle = styled.img`
+    width: 30px;
+    height: 30px;
+    margin-right: 10px;
+`
+
+function Login() {
+    return (
+        <Container>
+            <form id="login-form" className="p-4 px-5 rounded-4" style={{ backgroundColor: '#d2d2d2' }}>
+                <h1 className="text-center">Cadastro</h1>
+                <div className="my-3 mt-4">
+                    <input type="text" className="form-control" id="username" name="username" placeholder="USername" required />
+                </div>
+                <div className="my-3">
+                    <input type="email" className="form-control" id="email" name="email" placeholder="Email" required />
+                </div>
+                <div className="mb-3">
+                    <input type="password" className="form-control" id="password"  name="password" placeholder="Senha" required />
+                </div>
+                <div className="mb-3">
+                    <input type="password" className="form-control" id="confirm-password"  name="confirm-password" placeholder="Confirmar Senha" required />
+                </div>
+                <div className="text-center mb-3">
+                    <Button>Criar</Button>
+                </div>
+                <div className="text-center mb-3">
+                    <Button type="submit" className="btn-google bg-light text-dark">
+                        <div className="btn-content">
+                            <IconGoogle src="https://img.icons8.com/?size=100&id=17949&format=png&color=000000" /> Entrar com
+                            Google
+                        </div>
+                    </Button>
+                </div>
+            </form>
+        </Container>
+    );
+}
+
+export default Login;
